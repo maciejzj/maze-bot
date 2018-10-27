10abb2ce30a8f96d6fc0a6ad6621032e04fb310e
root <staszulonekb@gmail.com>
2018-10-27 18:05:15 +0200

    Create servoSensor turning function

--- src/servoSensor.cpp
+++ src/servoSensor.cpp
@@ -1,6 +1,21 @@
+#include <Servo.h>
 #include "servoSensor.h"
 #include "defines.h"
 
-void turnServoSensor(int direction) {
+extern Servo servoSensor;
 
+void turnServoSensor(int direction) {
+    switch(direction) {
+        case RIGHT:
+			servoSensor.write(0);
+			break;
+		
+		case FRONT:
+			servoSensor.write(90);
+			break;
+
+		case LEFT:
+			servoSensor.write(180);
+			break;
+    }
 }
