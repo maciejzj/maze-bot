f74e66b32bc1c2e99173fc0322792d2fc9fb1032
root <staszulonekb@gmail.com>
2018-10-27 18:34:45 +0200

    Configure servo GPIO pinout

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,15 @@
 #include <Arduino.h>
+#include <Servo.h>
 #include "defines.h"
 #include "servoSensor.h"
 
+int servoPin = 6;
+Servo servoSensor;
+
 void setup() {
 	Serial.begin(9600); // for debug
+
+	servoSensor.attach(servoPin);
 }
 
 void loop() {
