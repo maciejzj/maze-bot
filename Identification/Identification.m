09114ad36580f08d02193f9d44d5689f9adff50e
Maciej Ziaja <maciejzjg@gmail.com>
2019-01-07 14:20:48 +0100

    Analyse csv data and draw root locus

--- Identification/Identification.m
+++ Identification/Identification.m
@@ -1,3 +1,30 @@
+[kl, Tl] = motorIdentif('motorLeftIdentif.csv');
+title('Left motor step response under no load');
+[kll, Tll] = motorIdentif('motorLeftIdentifLoad.csv')
+title('Left motor step response under platform load');
+[kr, Tr] = motorIdentif('motorRightIdentif.csv')
+title('Right motor step response under no load');
+[krl, Trl] = motorIdentif('motorRightIdentifLoad.csv')
+title('Right motor step response under platform load');
+
+figure
+s = tf('s'); 
+motRight = kr / (s + Tr);
+pole = 0.005;
+motRight = motRight * (s + pole)/s;
+motRight.TimeUnit = 'milliseconds';
+rlocus(motRight)
+title('Root Locus pole < T');
+
+figure
+s = tf('s'); 
+motRight = kr / (s + Tr);
+pole = 0.05;
+motRight = motRight * (s + pole)/s;
+motRight.TimeUnit = 'milliseconds';
+rlocus(motRight)
+title('Root Locus pole > T');
+
 function [kv, T] = motorIdentif(csv_name)
     figure()
     objectResponse = csvread(csv_name);
