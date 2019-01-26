e6da6867c17b52150ee71b68ea71eff93ecc4b14
Maciej Ziaja <maciejzjg@gmail.com>
2019-01-26 19:46:48 +0100

    Spaces for comments alignement

--- src/platformMotors.cpp
+++ src/platformMotors.cpp
@@ -17,8 +17,8 @@ extern int motRightCounter;
 extern unsigned long motLeftDeltaTime;
 extern unsigned long motRightDeltaTime;
 
-unsigned long lastTimeLeft;		///< Time from last interrupt on left endcoder
-unsigned long lastTimeRight;	///< Time from last interrupt on right endcoder
+unsigned long lastTimeLeft;     ///< Time from last interrupt on left endcoder
+unsigned long lastTimeRight;    ///< Time from last interrupt on right endcoder
 
 void motorStop() {
 	motorLeftStop();
@@ -55,15 +55,15 @@ void platformTurn(int direction) {
 }
 
 void platformTurnAngle(int degree) {
-	int actuationLeft;				///< Actuation of the left motor, steers the motor's PWM.
-	int actuationRight;				///< Actuation of the right motor, steers the motor's PWM.
-	float Pv = 1.2;					///< Proportional coefficient of (subordinate) velocity regulator.
-	float Iv = 1.2;					///< Integral coefficient of (subordinate) velocity regulator.
-	float Pp = 0.35;				///< Proportional coefficient of (precedent) position regulator.
-	float setPointVeloRight;		///< Velocity set point for subordinate regulator of right motor.
-	float setPointVeloLeft;			///< Velocity set point for subordinate regulator of left motor.
-	bool leftIsStopped = false;		///< Indicator of left motor being stopped after it reaches posiotion set point.
-	bool rightIsStopped = false;	///< Indicator of right motor being stopped after it reaches posiotion set point.
+	int actuationLeft;       ///< Actuation of the left motor, steers PWM.
+	int actuationRight;      ///< Actuation of the right motor, steers PWM.
+	float Pv = 1.2;          ///< P coefficient of (subordinate) velocity controller.
+	float Iv = 1.2;          ///< I coefficient of (subordinate) velocity controller.
+	float Pp = 0.35;         ///< P coefficient of (precedent) position controller.
+	float setPointVeloRight; ///< Velocity set point for subordinate controller of right motor.
+	float setPointVeloLeft;  ///< Velocity set point for subordinate controller of left motor.
+	bool leftIsStopped = false;  ///< Stop indicator for left motor after reaching set point.
+	bool rightIsStopped = false; ///< Stop indicator for right motor after reaching set point.
 
 	/* Clear initial values for encoder readouts */
 	motLeftDeltaTime = 1;
@@ -78,13 +78,13 @@ void platformTurnAngle(int degree) {
 	/* Calculate direction sign, determines whether platform turns left or right */
 	int direction = degree > 0 ? 1 : -1;
 
-	/* Set the last time ISRs as the begging of the work of the regulator */
+	/* Set the last time ISRs as the begging of the work of the controller */
 	lastTimeLeft = millis();
 	lastTimeRight = millis();
 
 	while(1) {
-		int slotDeltaRight = target - motRightCounter;  ///< Error of position (precedent) regulator for right motor.
-		int slotDeltaLeft = target - motLeftCounter;    ///< Error of position (precedent) regulator for left motor.
+		int slotDeltaRight = target - motRightCounter;  ///< Error of position (precedent) controller for right motor.
+		int slotDeltaLeft = target - motLeftCounter;    ///< Error of position (precedent) controller for left motor.
 
 		/* P regualtor formula, calcualtes velocity setpoint */
 		setPointVeloRight = Pp * slotDeltaRight;
@@ -100,7 +100,7 @@ void platformTurnAngle(int degree) {
 		/* Get current time to compare it with last time of ISR and estimate integral */
 		unsigned long compareTime = millis();
 	
-		/* PI regulator formula, calculates motors actuations */
+		/* PI controller formula, calculates motors actuations */
 		actuationRight = round(Pv * veloDeltaRight + Iv * veloDeltaRight * (compareTime - lastTimeRight));
 		actuationLeft = round(Pv * veloDeltaLeft + Iv * veloDeltaLeft * (compareTime - lastTimeLeft));
 
@@ -159,19 +159,19 @@ void resetEncoders() {
 }
 
 void headingVeloFix() {
-	int actuationLeft;	///< Actuation of the left motor, steers the motor's PWM.
-	int actuationRight;	///< Actuation of the right motor, steers the motor's PWM.
+	int actuationLeft;  ///< Actuation of the left motor, steers the motor's PWM.
+	int actuationRight; ///< Actuation of the right motor, steers the motor's PWM.
 
-	float Pm = 10;		///< Proportional coefficient of master velocity regulator.
-	float Ps = 15;		///< Proportional coefficient of slave velocity regulator.
-	float Is = 5;		///< Integral coefficient of slave velocity regulator.
+	float Pm = 10;      ///< Proportional coefficient of master velocity controller.
+	float Ps = 15;      ///< Proportional coefficient of slave velocity controller.
+	float Is = 5;       ///< Integral coefficient of slave velocity controller.
 
-	/* Master regulator */
+	/* Master controller */
 	float veloDeltaLeft = ((float)200.0/20.0) - (200.0/((float) motLeftDeltaTime));
 	actuationLeft = 112 + round(Pm * veloDeltaLeft);
 	if(actuationLeft > 255) actuationLeft = 255;
 
-	/* Slave regulator */
+	/* Slave controller */
 	float veloDelta = ((200.0/((float) motLeftDeltaTime)) - (200.0/((float) motRightDeltaTime)));
 	int slotDelta = motLeftCounter - motRightCounter;
 	actuationRight = 88 + round(Ps * veloDelta + Is * slotDelta);
